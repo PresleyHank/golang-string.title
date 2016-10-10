3bdb3fcf8e7c11ccaca3c68623180a69dac8966c
Dennis Mwangi <dnjaramba@gmail.com>
2016-10-10 19:54:47 +0300

    new implementation

---
+++ zstring.go
@@ -0,0 +1,22 @@
+
+import (
+	"bytes"
+	"strings"
+)
+
+
+
+func ToTitle(str string) string {
+	arrWords := strings.Split(str, " ")
+	newString := func(str string, r rune, i int) string {
+		return str[:i] + string(r) + str[i + 1:]
+	}
+	var buffer bytes.Buffer
+	for i := 0; i < len(arrWords); i++ {
+		if (arrWords[i][0] >= 'a' && arrWords[i][0] <= 'z') {
+			buffer.WriteString(newString(arrWords[i], rune(arrWords[i][0]) - 32, 0))
+			buffer.WriteString(" ")
+		}
+	}
+	return buffer.String()
+}
